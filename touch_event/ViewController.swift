02a1ba17bfa1c96c7132aadb48afca9877d0f684
ajj-k <bouando4ha@docomo.ne.jp>
2022-11-05 00:58:51 +0900

    Initial Commit

---
+++ touch_event/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  touch_event
+//
+//  Created by Kiyoshi Ohashi on 2022/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
